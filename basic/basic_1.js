e7e5523d411157cc848d26fb5a5c2840a0ff7803
constructorlib <constructorlib@gmail.com>
2024-03-11 16:51:38 +0900

    init basic js + hooks + lrn

---
+++ basic/basic_1.js
@@ -0,0 +1,23 @@
+const data = [
+  {
+    id: "1",
+    title: "Tiramisu",
+    description: "Chocolate cake",
+    image: "example.com",
+    price: "$5",
+  },
+  {
+    id: "2",
+    title: "Lemon Ice Cream",
+    description: "Unexpected flavor",
+    image: "example.com",
+    price: "$4.50",
+  },
+  {
+    id: "3",
+    title: "Chocolate Mousse",
+    description: "Unexplored taste",
+    image: "example.com",
+    price: "6$",
+  },
+];
