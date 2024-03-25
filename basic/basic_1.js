45d725b1d21aef750d96865eb2b3c95ffd1d9b1b
constructorlib <constructorlib@gmail.com>
2024-03-25 23:44:48 +0900

    comment what I forgot

--- basic/basic_1.js
+++ basic/basic_1.js
@@ -42,3 +42,16 @@ function App() {
     </div>
   );
 }
+/*
+const numbers = [1, 2, 3, 4, 5];
+
+const doubledNumbers = numbers.map((num) => {
+  return num * 2;
+});
+
+console.log(doubledNumbers); 
+
+// Output: [2, 4, 6, 8, 10]
+
+
+*/
