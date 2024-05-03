f91dc4700b72cae773eab3a8b06b74d5e43e06e1
constructorlib <constructorlib@gmail.com>
2024-05-03 14:27:43 +0900

    array manipulation [1]

---
+++ w3test/date7.js
@@ -0,0 +1,19 @@
+/*
+
+Write a JavaScript program to compute the sum 
+of three elements of a given array of integers of length 3.
+
+*/
+
+function sum_three(nums) {
+  return nums[0] + nums[1] + nums[2];
+}
+
+function check_length(arr) {
+  if (arr.length === 3) {
+    console.log(sum_three(arr));
+  } else if (arr.length < 3) {
+    console.log("Fuck me");
+  }
+}
+check_length([1, 2, 3]);
