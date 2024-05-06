8f1b93c984c025ac323da36f65dd89504419b9ad
constructorlib <constructorlib@gmail.com>
2024-05-07 00:28:38 +0900

    optimize

--- basic/basic_8.js
+++ basic/basic_8.js
@@ -1,8 +1,6 @@
 function error_handler(a, b) {
   if (typeof a === "number" && typeof b === "number") {
     return a + b;
-  } else if (typeof a === "number" && typeof b === "number") {
-    return "Not a number!";
   } else {
     return "Not a number!";
   }
